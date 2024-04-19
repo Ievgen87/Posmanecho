98460e356ff7ac77874d86815f5922ae639a4f63
Ромашко <romashko-bor@mail.ru>
2024-04-19 17:29:24 +0300

    failed test

--- src/test/java/PostmanechoTest.java
+++ src/test/java/PostmanechoTest.java
@@ -18,7 +18,7 @@ public class PostmanechoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data",equalTo("some data"))
+                .body("data",equalTo("sos data"))
         ;
     }
 }
